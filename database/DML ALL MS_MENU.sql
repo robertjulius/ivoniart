65d57f408fb0762f97d7bb257e70e9dbc5c4f2c4
robert.julius.ext <robert.julius.ext@W7-IDJKT0089.wincor-nixdorf.com>
2013-04-22 23:46:05 +0700

    DML untuk initialisasi ms_menu_services

--- database/DML ALL MS_MENU.sql
+++ database/DML ALL MS_MENU.sql
@@ -1,2 +1,5 @@
 DELETE FROM ms_menu_about;
 INSERT INTO ms_menu_about (id, create_by, create_date, rec_status, update_by, update_date, content, title, picture_id) VALUES ('1', 'SYSTEM', CURRENT_TIMESTAMP, 'A', 'SYSTEM', CURRENT_TIMESTAMP, 'This paragraph is the content of menu about', 'About', '');
+
+DELETE FROM ms_menu_services;
+INSERT INTO ms_menu_services (id, create_by, create_date, rec_status, update_by, update_date, content, title, picture_id) VALUES ('1', 'SYSTEM', CURRENT_TIMESTAMP, 'A', 'SYSTEM', CURRENT_TIMESTAMP, 'This paragraph is the content of menu services', 'Services', '');
