615aa33d5c77730fe6ca73821e7ba9040346d8d9
robert.julius.ext <robert.julius.ext@W7-IDJKT0089.wincor-nixdorf.com>
2013-04-22 11:35:36 +0700

    UserGroup dan User tidak perlu ada listAccessPaths

--- src/main/java/com/ganesha/basicweb/modules/usergroupmaintenance/action/UserGroupMaintenanceAction.java
+++ src/main/java/com/ganesha/basicweb/modules/usergroupmaintenance/action/UserGroupMaintenanceAction.java
@@ -1,7 +1,5 @@
 package com.ganesha.basicweb.modules.usergroupmaintenance.action;
 
-import java.util.List;
-
 import com.ganesha.basicweb.modules.usergroupmaintenance.UserGroupMaintenanceBL;
 import com.ganesha.basicweb.modules.usergroupmaintenance.UserGroupMaintenanceForm;
 import com.ganesha.core.exception.AppException;
@@ -12,17 +10,7 @@ public abstract class UserGroupMaintenanceAction extends
 
 	private static final long serialVersionUID = 8114275581397242184L;
 
-	private List<String> listAccessPaths;
-
 	public UserGroupMaintenanceAction() throws AppException {
 		super(UserGroupMaintenanceForm.class, UserGroupMaintenanceBL.class);
-	}
-
-	public List<String> getListAccessPaths() {
-		return listAccessPaths;
-	}
-
-	public void setListAccessPaths(List<String> listAccessPaths) {
-		this.listAccessPaths = listAccessPaths;
 	}
 }
--- src/main/java/com/ganesha/basicweb/modules/usermaintenance/action/UserMaintenanceAction.java
+++ src/main/java/com/ganesha/basicweb/modules/usermaintenance/action/UserMaintenanceAction.java
@@ -1,7 +1,5 @@
 package com.ganesha.basicweb.modules.usermaintenance.action;
 
-import java.util.List;
-
 import com.ganesha.basicweb.modules.usermaintenance.UserMaintenanceBL;
 import com.ganesha.basicweb.modules.usermaintenance.UserMaintenanceForm;
 import com.ganesha.core.exception.AppException;
@@ -12,17 +10,7 @@ public abstract class UserMaintenanceAction extends
 
 	private static final long serialVersionUID = 8114275581397242184L;
 
-	private List<String> listAccessPaths;
-
 	public UserMaintenanceAction() throws AppException {
 		super(UserMaintenanceForm.class, UserMaintenanceBL.class);
-	}
-
-	public List<String> getListAccessPaths() {
-		return listAccessPaths;
-	}
-
-	public void setListAccessPaths(List<String> listAccessPaths) {
-		this.listAccessPaths = listAccessPaths;
 	}
 }
